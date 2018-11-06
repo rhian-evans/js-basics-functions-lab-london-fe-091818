e72767a2c063616e714f2cd4df65c1eeed9541f4
Rhian Evans <rhian.evans100@btinternet.com>
2018-11-06 19:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+  const  hq = 42;
+  if (block < hq) {
+    return hq - block;
+  }
+  return block - hq;
+}
+
+function distanceFromHqInFeet (block) {
+  const feetConversion = 264;
+   return distanceFromHqInBlocks(block) * feetConversion;
+}
+  
+function distanceTravelledInFeet (startBlock, endBlock) {
+  const feetConversion = 264;
+    const distance = startBlock - endBlock; {
+    if (endBlock > startBlock)
+    return endBlock - startBlock;
+  }
+  return distance * feetConversion
+}
+
+function calculateFarePrice (startBlock, endBlock) {
+  const feetConversion = 264;
+  switch (calculateFarePrice) {
+case (calculateFarePrice(400<=0)):
+return 'noCharge';
+
+case (calculateFarePrice(2000<=401)):
+return '2c *distance';
+
+case (calculateFarePrice(2500<=2000)):
+return 'flatFare';
+
+case(calculateFarePrice(distance<2500)):
+return 'cannot travel that far';
+}
+}
